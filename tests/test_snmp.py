4b7e63213c739cd4e3ae6f68a5d64400a6d37e61
jpmondet <jp@mondet.org>
2021-08-12 10:14:14 +0200

    [tests] Add snmp active snmp tests with snmpsim

--- tests/test_snmp.py
+++ tests/test_snmp.py
@@ -3,127 +3,52 @@
 import sys, os
 import json
 import pytest
+import asyncio
 from fastapi.exceptions import HTTPException
 from add_fake_data_to_db import delete_all_collections_datas, add_fake_datas
 
 sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/../backend/"))
 from api_for_frontend import get_graph, stats, neighborships
-from db_layer import prep_db_if_not_exist
+from db_layer import get_all_nodes, get_all_links, get_stats_devices
+from snmp_functions import (
+        NEEDED_MIBS_FOR_LLDP,
+        NEEDED_MIBS_FOR_STATS,
+        IFACES_TABLE_TO_COUNT,
+        get_snmp_creds
+)
+from snmp_get_lldp_topo import get_device_lldp_infos
+from snmp_get_ifaces_stats import get_stats_and_dump
 
 
-delete_all_collections_datas()
-prep_db_if_not_exist()
-add_fake_datas(12, 5)
+def test_getting_lldp_infos():
 
-TEST_GRAPH_DATA = {}
-with open("tests/graph_datas.json") as graph_datas:
-    TEST_GRAPH_DATA = json.load(graph_datas)
+    node_to_retrieve: str = "127.0.0.1"
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(
+    [ get_device_lldp_infos('127.0.0.1',
+        NEEDED_MIBS_FOR_LLDP.values(),
+        get_snmp_creds(snmp_user='lldp'), 
+        '127.0.0.1', 1161)
+    ]))
 
-TEST_STATS_DATA = {}
-with open("tests/stats_datas.json") as stats_datas:
-    TEST_STATS_DATA = json.load(stats_datas)
+    nodes = list(get_all_nodes())
 
-TEST_NEIGHS_DATA = {}
-with open("tests/neighs_datas.json") as neighs_datas:
-    TEST_NEIGHS_DATA = json.load(neighs_datas)
+    assert nodes[0]["device_name"] == node_to_retrieve
 
+def test_getting_iface_stats():
 
-def test_graph_nodes():
-    graph = get_graph()
-    assert graph["nodes"] == TEST_GRAPH_DATA["nodes"]
+    node_to_retrieve: str = "127.0.0.1"
+    iface_to_retrieve: str = "1/1"
 
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.wait(
+    [ get_stats_and_dump('127.0.0.1',
+        NEEDED_MIBS_FOR_STATS.values(),
+        get_snmp_creds(snmp_user='ifmib'), 
+        IFACES_TABLE_TO_COUNT,
+        '127.0.0.1', 1161)
+    ]))
 
-def test_graph_links():
-    graph = get_graph()
-    sorted_links = sorted(graph["links"],key=lambda d: (d["source"], d["target"]))
-    sorted_test_links = sorted(TEST_GRAPH_DATA["links"], key=lambda d: (d["source"], d["target"]))
-    # assert graph["links"] == TEST_GRAPH_DATA["links"]
-    assert sorted_links == sorted_test_links
+    stats = list(get_stats_devices([node_to_retrieve]))
 
-
-def test_stats_of_link_between_2_devices():
-    query = ["fake_device_stage1_1", "fake_device_stage1_2"]
-    stats_retrieved = stats(query)
-    # Cant check timestamp since test datas in json are static
-    for device_datas in stats_retrieved.values():
-        for iface_datas in device_datas.values():
-            for stat in iface_datas["stats"]:
-                stat["time"] = "N/A"
-
-    print(json.dumps(stats_retrieved))
-    assert stats_retrieved == TEST_STATS_DATA
-
-
-def test_stats_of_1_device():
-    query = ["fake_device_stage1_1"]
-    stats_retrieved = stats(query)
-    # Cant check timestamp since test datas in json are static
-    for device_datas in stats_retrieved.values():
-        for iface_datas in device_datas.values():
-            for stat in iface_datas["stats"]:
-                stat["time"] = "N/A"
-
-    print(json.dumps(stats_retrieved))
-
-    test_datas_to_match = {
-        "fake_device_stage1_1": {
-            "1/1": {
-                "ifDescr": "1/1",
-                "index": "1/1",
-                "stats": [{"InSpeed": 0, "OutSpeed": 0, "time": "N/A"}],
-            },
-            "1/2": {
-                "ifDescr": "1/2",
-                "index": "1/2",
-                "stats": [{"InSpeed": 0, "OutSpeed": 0, "time": "N/A"}],
-            },
-            "1/3": {
-                "ifDescr": "1/3",
-                "index": "1/3",
-                "stats": [{"InSpeed": 0, "OutSpeed": 0, "time": "N/A"}],
-            },
-            "1/4": {
-                "ifDescr": "1/4",
-                "index": "1/4",
-                "stats": [{"InSpeed": 0, "OutSpeed": 0, "time": "N/A"}],
-            },
-        }
-    }
-    assert stats_retrieved == test_datas_to_match
-
-
-def test_stats_bad_request_not_list():
-    # Query should be a list
-    query = "fake_device_stage1_1"
-    with pytest.raises(HTTPException):
-        stats_retrieved = stats(query)
-
-
-def test_stats_bad_request_not_str_list():
-    # Query should be a list of str
-    query = [1]
-    with pytest.raises(HTTPException):
-        stats_retrieved = stats(query)
-
-
-def test_neighborships():
-    query = "fake_device_stage1_1"
-
-    neighs = neighborships(query)
-    print(json.dumps(neighs))
-
-    assert neighs == TEST_NEIGHS_DATA
-
-
-def test_neighborships_bad_request_with_int():
-    # Query should be a str
-    query = 1
-    with pytest.raises(HTTPException):
-        neighs = neighborships(query)
-
-
-def test_neighborships_bad_request_with_list():
-    # Query should be a str
-    query = ["test"]
-    with pytest.raises(HTTPException):
-        neighs = neighborships(query)
+    assert len(stats) == 8
