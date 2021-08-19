63b0ed97e059b199a20c0ad6aafc2eea65d0e3f5
jpmondet <jp@mondet.org>
2021-08-19 08:34:25 +0200

    [tests][snmp_funcs] Fix pylint/mypy last warnings

--- tests/test_snmp.py
+++ tests/test_snmp.py
@@ -1,29 +1,28 @@
+"""This module aims to test the snmp functions with pytest.
+Warning: A mongodb and a snmpsim node must be up&running"""
 #! /bin/env python3
 
 import sys
 import os
-import json
-import pytest
-import asyncio
-from fastapi.exceptions import HTTPException
-from add_fake_data_to_db import delete_all_collections_datas, add_fake_datas
+from typing import Dict, List, Any
+from add_fake_data_to_db import delete_all_collections_datas
 
 sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/../backend/"))
-from api_for_frontend import get_graph, stats, neighborships
-from db_layer import prep_db_if_not_exist, get_node, get_all_links, get_stats_devices
+# pylint:disable=import-error, wrong-import-position
+from db_layer import prep_db_if_not_exist, get_node, get_stats_devices
 from snmp_functions import (
-    NEEDED_MIBS_FOR_LLDP,
-    NEEDED_MIBS_FOR_STATS,
-    IFACES_TABLE_TO_COUNT,
     get_snmp_creds,
 )
-from snmp_get_lldp_topo import get_device_lldp_infos, lldp_scrapping
-from snmp_get_ifaces_stats import get_stats_and_dump, stats_scrapping
+from snmp_get_lldp_topo import lldp_scrapping
+from snmp_get_ifaces_stats import stats_scrapping
 
 SNMP_NODE_TO_RETRIEVE: str = os.getenv("SNMP_NODE_TO_RETRIEVE", "127.0.0.1")
 
 
-def test_getting_lldp_infos():
+def test_getting_lldp_infos() -> None:
+    """Launch the main lldp scrapping function on a
+    dumb node and checks db to see if it was correctly scrapped
+    and dumped"""
 
     delete_all_collections_datas()
     prep_db_if_not_exist()
@@ -33,13 +32,16 @@ def test_getting_lldp_infos():
     assert get_node(SNMP_NODE_TO_RETRIEVE)
 
 
-def test_getting_iface_stats():
+def test_getting_iface_stats() -> None:
+    """Launch the main stats scrapping function on a
+    dumb node and check db to see if stats were correctly scrapped
+    and dumped"""
 
     delete_all_collections_datas()
     prep_db_if_not_exist()
 
     stats_scrapping(get_snmp_creds(snmp_user="ifmib"), SNMP_NODE_TO_RETRIEVE)
 
-    stats_from_db = list(get_stats_devices([SNMP_NODE_TO_RETRIEVE]))
+    stats_from_db: List[Dict[str, Any]] = list(get_stats_devices([SNMP_NODE_TO_RETRIEVE]))
 
     assert len(stats_from_db) == 8
